8e4f958a84dbf5b791b7bc5cc1747637cf8e0986
Ghenadie Isacenco <isacenco@gmail.com>
2025-10-23 15:21:29 +0200

    Created Addaptive Carrousel

---
+++ Touchdown/Model/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  Touchdown
+//
+//  Created by Ghenadie Isacenco on 23/10/2025.
+//
+
+import Foundation
+
+struct Player: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
--- Touchdown/Utility/Constant.swift
+++ Touchdown/Utility/Constant.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 // DATA
+
+let players: [Player] = Bundle.main.decode("player.json")
+
 // COLOR
 let colorBackground: Color = Color("colorBackground")
 let colorGray: Color = Color(UIColor.systemGray4)
