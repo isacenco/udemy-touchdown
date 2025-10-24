065d343948d5692cc4bdd1b1c1538b65d929bd88
Ghenadie Isacenco <isacenco@gmail.com>
2025-10-24 10:29:59 +0200

    Part 1 Completed

---
+++ Touchdown/App/ProductDetailView.swift
@@ -0,0 +1,50 @@
+//
+//  ProductDetailView.swift
+//  Touchdown
+//
+//  Created by Ghenadie Isacenco on 24/10/2025.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    // MARK: - PROPERTIES
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5) {
+            // NAVBAR
+            NavigationBarDetailView()
+                .padding(.horizontal)
+                .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+            
+            
+            // HEADER
+            HeaderDetailView()
+                .padding(.horizontal)
+            
+            // DETAIL TOP PART
+            TopPartDetailView()
+                .padding(.horizontal)
+            
+            // DETAIL BOTTOM PART
+            
+            // RATINGS + SIZES
+            
+            // DESCRIPTION
+            
+            // QUANTITY + FAVORITE
+            
+            // ADD TO CART
+            Spacer()
+        } //: VSTACK
+        .ignoresSafeArea(.all, edges: .all)
+        .background(
+            Color(red: sampleProduct.red, green: sampleProduct.green, blue: sampleProduct.blue)
+                .ignoresSafeArea(.all, edges: .all)
+        )
+    }
+}
+
+#Preview(traits: .fixedLayout(width: 375, height: 812)) {
+    ProductDetailView()
+}
---
+++ Touchdown/View/Detail/TopPartDetailView.swift
@@ -0,0 +1,48 @@
+//
+//  TopPartDetailView.swift
+//  Touchdown
+//
+//  Created by Ghenadie Isacenco on 24/10/2025.
+//
+
+import SwiftUI
+
+struct TopPartDetailView: View {
+    // MARK: - PROPERTIES
+    @State private var isAnimating: Bool = false
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 6) {
+            // PRICE
+            VStack(alignment: .leading, spacing: 6) {
+                Text("Price")
+                    .fontWeight(.semibold)
+                
+                Text(sampleProduct.formattedPrice)
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .scaleEffect(1.35, anchor: .leading)
+            } //: VSTACK
+            .offset(y: isAnimating ? -50 : -75)
+            
+            Spacer()
+            
+            // PHOTO
+            Image(sampleProduct.image)
+                .resizable()
+                .scaledToFit()
+                .offset(y: isAnimating ? 0 : -35)
+            
+        } //: HSTACK
+        .onAppear {
+            withAnimation(.easeOut(duration: 0.75)) {
+                isAnimating.toggle()
+            }
+        }
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    TopPartDetailView()
+        .padding()
+}
